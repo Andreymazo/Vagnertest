21f6b7a86514171d6ce31206e8be4e314537f1a3
andrey_mazo <andreymazo@mail.ru>
2024-05-21 03:09:01 +0300

    first commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,55 @@
-$(function() {
-    $('.slider').slick()});
+// $(function() {
+//     $('.slider').slick(
+//         {
+//             arrows:true,
+//             dots:true
+//         }
+//     )});
+    (function($){
+        $('.slider').slick(
+            {
+                arrows:true,
+                dots:true,
+                adaptiveHeight:true,
+                slidesToShow:1,
+                slidesToScroll:1,
+                speed:100,
+                easing:'linear',
+                infinite:true,
+                autoplay:false,
+                autoplayspeed:1000,
+                pauseOnFocus:true,
+                pauseOnHover:true,
+                pauseOnDotHover:true,
+                draggable:true,
+                swipe:true,
+                touchThresdhold:5,
+                touchMove:true,
+                waitForAnimate:false,
+                centerMode:false,
+                variableWidth: false,
+                rows:1,
+                slidesPerRow:1,
+                vertical: false,
+                asNavFor: '.slider_big'
+            }
+
+        );
+      })(jQuery);
+
+      (function($){
+        $('.slider_big').slick(
+            {   
+                
+                arrows:false,
+                dots:false,
+                centerMode:true,
+                asNavFor: '.slider'
+           
+            }
+
+        );
+      })(jQuery);
+
+
 
